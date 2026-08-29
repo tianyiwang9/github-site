2a806602d356d6fff6b96ede0268ef0706a514e6
tianyiwang9 <tianyiwang919@gmail.com>
2026-08-28 20:57:57 -0400

    Publish Rising Sea solution 1.2.E

--- rising-sea-solutions.js
+++ rising-sea-solutions.js
@@ -110,7 +110,8 @@ Indeed, define $\psi: S^{-1}A\to B$ via $\psi(a/s)=\phi(a)\phi(s)^{-1}$. It is e
 % https://q.uiver.app/#q=WzAsMyxbMCwwLCJNXzFcXHRpbWVzXFxjZG90c1xcdGltZXMgTV9uIl0sWzIsMCwiU157LTF9TV8xXFx0aW1lc1xcY2RvdHNcXHRpbWVzIFNeey0xfU1fbiJdLFsxLDEsIk4iXSxbMCwyLCJcXHBoaSIsMl0sWzAsMV0sWzEsMiwiXFxleGlzdHMhXFxwc2kiLDAseyJzdHlsZSI6eyJib2R5Ijp7Im5hbWUiOiJkYXNoZWQifX19XV0=
 where the verticle map is the obvious one $(m_1,...,m_n)\mapsto (m_1/1,...,m_n/1).$ Now let $e_1,...,e_n$ denote the standard basis in $M_1\times\cdots\times M_n$. Then we see that we can define
 \begin{align*}
-\psi(m_1/s_1,...,m_n/s_n)=\psi(\sum_{1\leq i\leq n}(0,...,m_i/s_i,...,0))=\sum_{1\leq i\leq n}\phi(s_i)^{-1}\phi(0,...,m_i,...,0).
+\psi(m_1/s_1,...,m_n/s_n)&=\psi(\sum_{1\leq i\leq n}(0,...,m_i/s_i,...,0))\\
+&=\sum_{1\leq i\leq n}\phi(s_i)^{-1}\phi(0,...,m_i,...,0).
 \end{align*}`
   },
   {
